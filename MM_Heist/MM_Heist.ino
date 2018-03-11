4514452c3cae8c278a495328723b99299538eddb
daugihao <wojreynolds@gmail.com>
2018-03-11 12:58:41 +0000

    Fake temperature output and buttons configured properly

--- MM_Heist/MM_Heist.ino
+++ MM_Heist/MM_Heist.ino
@@ -3,44 +3,80 @@
 // variable declarations and initialisations
 const float pi = 3.14159;
 
-float timestep = 1; // milliseconds
+float timestep = 1.0; // milliseconds
 
-float rangeDAC = 4095;
+float rangeDAC = 4095.0;
 int settingDAC =0;
 
+float alarmSecs = 0.0;
+float alarmLimitSecs = 10.0;
 float inputTimeSine = 0.0;
 float outputTimeSine = 0.0;
-
 float inputFreqSine = 0.0;
 float outputFreqSine = 0.0;
-float frequencyFreqSine = 2;
-float minFreqSine = 25;
-float maxFreqSine = 500;
+float frequencyFreqSine = 2.0;
+float minFreqSine = 25.0;
+float maxFreqSine = 500.0;
+
+float timeSecs = 0.0;
+float timeLimitSecs = 30.0;
+float tempDegrees = 0.0;
+float minTemp = 18.0;
+float maxTemp = 25.0;
+
+bool buttonState = 0;
 
 ///////////////////////////////////////////////////////////////////////
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // maximise resolution of the DACs
+  // maximise resolution of the DACs (DAC0)
   analogWriteResolution(12);
   // enable serial communications for console
   Serial.begin(9600); // open the serial port at 9600 bps
+  // set up button inputs (52,53)
+  pinMode(52, INPUT_PULLUP);
+  pinMode(53, INPUT_PULLUP);
+  attachInterrupt(52, heistReset, FALLING);
+  attachInterrupt(53, heistTrigger, FALLING);
+  tempDegrees = minTemp;
+  timeSecs = 0.0;
+  alarmSecs = 0.0;
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  inputFreqSine += (2*pi*frequencyFreqSine*timestep/1000.0);
-  outputFreqSine = (0.5*(sin(inputFreqSine)+1.0))*(maxFreqSine-minFreqSine) + minFreqSine;
-  
-  inputTimeSine += (2*pi*outputFreqSine*timestep/1000.0);
-  outputTimeSine = 0.5*(sin(inputTimeSine)+1.0);
-  settingDAC = outputTimeSine*rangeDAC;
-  delay(timestep);
-  
-  analogWrite(DAC0, settingDAC); 
 
-//Serial.print(settingDAC);
-//Serial.print("\n\r");
+  if (timeSecs < timeLimitSecs) {
+    timeSecs++;
+    tempDegrees = (maxTemp-minTemp)*(timeSecs/timeLimitSecs) + minTemp;
+    delay(1000);
 
+    Serial.print(tempDegrees,2);
+    Serial.print("\n\r");
+  }
+  else if ((timeSecs >= timeLimitSecs) & (alarmSecs < alarmLimitSecs)) {
+    alarmSecs += timestep/1000.0;
+    inputFreqSine += (2*pi*frequencyFreqSine*timestep/1000.0);
+    outputFreqSine = (0.5*(sin(inputFreqSine)+1.0))*(maxFreqSine-minFreqSine) + minFreqSine;
+    
+    inputTimeSine += (2*pi*outputFreqSine*timestep/1000.0);
+    outputTimeSine = 0.5*(sin(inputTimeSine)+1.0);
+    settingDAC = outputTimeSine*rangeDAC;
+    delay(timestep);
+    
+    analogWrite(DAC0, settingDAC); 
+  }
+
+}
+
+void heistReset() {
+  timeSecs = 0.0;
+  alarmSecs = 0.0;
+}
+
+void heistTrigger() {
+  timeSecs = timeLimitSecs;
+  alarmSecs = 0.0;
 }
 
